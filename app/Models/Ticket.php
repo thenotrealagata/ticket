d3dd81e6bd01d4d1f97c0c6cfd730daa5c911b7d
Fábián Ágota <agota.fabian16@gmail.com>
2024-09-26 21:56:25 +0200

    Gyak02

---
+++ app/Models/Ticket.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'done',
+        'priority',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'done' => 'boolean'
+        ];
+    }
+
+    public function comments(): HasMany {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function users(): BelongsToMany {
+        return $this->belongsToMany(User::class);
+    }
+}
